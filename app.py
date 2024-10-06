26f00bdc1cf02da28191c4edba996fca74dab91e
Ekaterina_Varlamova <katy1781@inbox.ru>
2024-10-07 01:54:49 +0300

    added auth

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO
 import random
 import time
@@ -11,13 +11,12 @@ from collections import deque
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-
 HOST = 'localhost'
 PORT = 5001
-USERNAME = "client1"
-PASSWORD = "password1"
 MAX_BUFFER_SIZE = 5
-
+username = None
+password = None
+auth = False
 
 def extract_json_and_buffer(input_string):
     if ";" not in input_string:
@@ -30,37 +29,53 @@ def extract_json_and_buffer(input_string):
     return objs, end
 
 def connect_to_server():
+    while not auth:
+        pass
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
 
-    client.sendall(f"{USERNAME},{PASSWORD}".encode())
+    client.sendall(f"{username},{password}".encode())
     response = client.recv(1024).decode()
-    if response != "Authorized":
-        print("Authorization failed")
-        return
 
-    print("Connected to the server")
+    buf = ""
+    last = 0
+    while True:
+        data = client.recv(1024).decode()
+        if not data:
+            break
+        buf += data
+        extracted, buf = extract_json_and_buffer(buf)
+        for ext in extracted:
+            latitude = float(ext["latitude"])
+            longitude = float(ext["longitude"])
+            socketio.emit('new_position', {'lat': latitude, 'lon': longitude})
 
-    try:
-        buf = ""
-        last = 0
-        while True:
-            data = client.recv(1024).decode()
-            if not data:
-                break
-            buf += data
-            extracted, buf = extract_json_and_buffer(buf)
-            for ext in extracted:
-                latitude = float(ext["latitude"])
-                longitude = float(ext["longitude"])
-                socketio.emit('new_position', {'lat': latitude, 'lon': longitude})
 
-    finally:
-        client.close()
-        
+@app.route("/login", methods=["POST"])
+def login():
+    global username
+    global password
+    global auth
+    username = "client1" #request.form["username"]
+    password = "password1" #request.form["password"]
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+
+    client.sendall(f"{username},{password}".encode())
+    response = client.recv(1024).decode()
+    client.close()
+    if response == "Authorized":
+        auth = True
+        return render_template('index.html')
+    else:
+        return render_template("login.html")
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if auth:
+        return render_template('index.html')
+    else:
+        return render_template("login.html")
 
 if __name__ == '__main__':
     threading.Thread(target=connect_to_server).start()
